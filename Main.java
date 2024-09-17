c2c41a25e0667735deb936a3fbbce81305f9c8cc
Estrella Sanchez <l24120345@morelia.tecnm.mx>
2024-09-16 21:17:35 -0600

    Tarea 7

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Creación de Develop");
-
-    }}
+        System.out.println("Clase producto de la tarea 7");
+   }}
