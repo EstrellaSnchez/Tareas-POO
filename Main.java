422bab6d93175f1813dbc1aaa5eb22c6fe2a8622
Estrella Sanchez <l24120345@morelia.tecnm.mx>
2024-11-13 22:31:21 -0600

    Tarea 1

--- Main.java
+++ Main.java
@@ -1,5 +1,59 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Creación de Develop");
+        Scanner sc = new Scanner(System.in);
+        int opcion=0;
 
-    }}
+        while(opcion!=3){
+            System.out.println("==CUADERNO==");
+            System.out.println("1. AGREGAR TAREA");
+            System.out.println("2. MOSTRAR TAREAS");
+            System.out.println("3. SALIR ");
+            try{
+                opcion=sc.nextInt();
+                sc.nextLine();
+                switch(opcion){
+                    case 1:
+                        System.out.println("--INGRESE UNA TAREA--");
+                        try (FileWriter writer = new FileWriter("ListaDeTareas.txt", true); BufferedWriter bw = new BufferedWriter(writer)){
+                            System.out.print("INGRESE LA TAREA: ");
+                            String tarea = sc.nextLine();
+                            bw.write(tarea + "\n");
+                        }catch (IOException e){
+                            System.out.println("ERROR AL SOBREESCRIBIR EL ARCHIVO" + e.getMessage());
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println("--LISTA TAREAS--");
+                        try (BufferedReader read = new BufferedReader(new FileReader("ListaDeTareas.txt"))) {
+                            String lectura;
+                            while ((lectura = read.readLine()) != null) {
+                                System.out.println(lectura);
+                            }
+                        } catch (IOException e){
+                            System.out.println("ERROR AL LEER EL ARCHIVO" + e.getMessage());
+                        }
+                        break;
+
+                    case 3:
+                        System.out.print("CERRANDO PROGRAMA . . .");
+                        break;
+
+                    default:
+                        System.out.println("OPCION INVALIDA");
+
+
+                }
+
+            }catch(Exception e){
+                System.out.println("Ingresa un numero: ");
+                sc.nextLine();
+
+            } 
+
+        }
+}
+}
